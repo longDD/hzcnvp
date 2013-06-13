ab5f5dfd1c73e8d8c892c86b7c5ca0ca037aef0f
michaelzx <mail@zhangxiao.org>
2013-06-14 01:10:38 +0800

    rbac 移植
    Signed-off-by: michaelzx <mail@zhangxiao.org>

--- CNVP/Conf/config.php
+++ CNVP/Conf/config.php
@@ -1,5 +1,10 @@
 <?php
-return array(
+$database = require 'sys.db.php';
+$rbac =  require 'sys.rbac.php';
+$config = array(
 	'SHOW_PAGE_TRACE'=>true,
+    'URL_MODEL'                 =>  2, // 如果你的环境不支持PATHINFO 请设置为3    
 );
+$r = array_merge($database,$rbac,$config);
+return $r;
 ?>
---
+++ CNVP/Conf/sys.db.php
@@ -0,0 +1,11 @@
+<?php 
+return array(
+	'DB_TYPE'                   =>  'mysql',
+    'DB_HOST'                   =>  'cnvpzw.gotoftp2.com',
+    'DB_NAME'                   =>  'cnvpzw',
+    'DB_USER'                   =>  'cnvpzw',
+    'DB_PWD'                    =>  '[zhengwei12]',
+    'DB_PORT'                   =>  '3306',
+    'DB_PREFIX'                 =>  'cnvp_',
+);
+ ?>
